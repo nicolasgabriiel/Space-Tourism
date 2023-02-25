29a3584d0a14c4698e2233660bdf0356497e1f88
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-24 21:40:38 -0300

    Criando Página de Destino

---
+++ src/views/Destination.vue
@@ -0,0 +1,15 @@
+<template>
+    <HeaderHome/>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HeaderHome from '@/components/HeaderHome.vue';
+
+export default defineComponent({
+    name: 'Destination-Page',
+    components:{
+        HeaderHome
+    }
+
+})
+</script>
