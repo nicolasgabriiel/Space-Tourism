2c1b54090676b6cbcdfaf838ab51e2abc887a937
Nicolas <nicolasgabriel.developer@gmail.com>
2023-03-27 21:25:49 -0300

    FEAT: TechnologyImage
    Criando novo componente pra armazenar as imagens da página

---
+++ src/components/TechnologyImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <div class="image image1"></div>
+    <div class="image image2"></div>
+    <div class="image image3"></div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "TechnologyImage",
+});
+</script>
+
+<style></style>
