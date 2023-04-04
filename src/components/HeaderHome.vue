dc01632338ec3239008315b72e68d0d79aaa6ca6
Nicolas <nicolasgabriel.developer@gmail.com>
2023-04-04 20:29:34 -0300

    STYLE: RESPONSIVO

--- src/components/HeaderHome.vue
+++ src/components/HeaderHome.vue
@@ -118,5 +118,60 @@ strong {
 /* RESPONSIVO */
 
 @media screen and (max-width: 800px) {
+  header {
+    display: flex;
+    width: 95%;
+    margin-left: 5%;
+    height: 96px;
+    padding-top: 40px;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .linha {
+    width: 473px;
+    height: 1px;
+    background: white;
+    opacity: 0.25;
+    display: absolute;
+    margin-left: 300px;
+    z-index: 999;
+  }
+  .nav-header {
+    width: 70%;
+    background: rgba(255, 255, 255, 0.04);
+    backdrop-filter: blur(40.7742px);
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+  .botoes-header:first-child {
+    margin-left: 100px;
+  }
+  .botoes-header {
+    background: none;
+    border: none;
+    margin-right: 70px;
+    padding-bottom: 33px;
+    cursor: pointer;
+  }
+  .botoes-header:hover {
+    border-bottom: 4px solid #cccccc;
+  }
+  strong {
+    font-weight: bold;
+    margin-right: 20px;
+  }
+  .descricao-header {
+    margin-top: 35px;
+    color: white;
+    font-size: 19px;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-family: "Barlow Condensed", sans-serif;
+  }
+  .active {
+    border-bottom: 4px solid white;
+  }
 }
 </style>
